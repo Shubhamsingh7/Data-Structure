c25a02051288d76ee72b0a797c5b1a009000b539
Shubham singh <singh6shubham@gmail.com>
2020-05-01 16:08:15 +0530

    removed .exe

--- 01LinkedList/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-
-    list->insertData(21);
-    list->insertData(96);
-    list->insertData(1);
-    list->insertData(73);
--- 04Tree/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-,8,6,25,3,6,89,12,0,7};
-    // tree->insert(array);
-    // tree->display();
