a93cffedbeb9d6a7d50b151688dabc1c0ed51845
Shubham singh <singh6shubham@gmail.com>
2020-05-12 04:20:13 +0530

    dijkstra

--- 09Dijkstra/dijekstra.cpp
+++ 09Dijkstra/dijekstra.cpp
@@ -53,13 +53,35 @@ class Graph{
 };
 int main(){
 
-    Graph<int> g;
-    g.addEdge(1,2,1);
-    g.addEdge(2,3,8);
-    g.addEdge(1,3,4);
-    g.addEdge(1,4,7);
-    g.addEdge(3,4,2);
-    g.dijkstra(1);
+    // Graph<int> g;
+    // g.addEdge(1,2,1);
+    // g.addEdge(2,3,8);
+    // g.addEdge(1,3,4);
+    // g.addEdge(1,4,7);
+    // g.addEdge(3,4,2);
+    // g.dijkstra(1);
+    
+    Graph<string> g;
+    g.addEdge("delhi","lucknow",5);
+    g.addEdge("delhi","chandigardh",4);
+    g.addEdge("kolkata","mumbai",2);
+    g.addEdge("delhi","jaipur",4);
+    g.addEdge("lucknow","kolkata",7);
+    g.addEdge("chandigardh","amritsir",2);
+    g.addEdge("jammu","amritsir",4);
+    g.addEdge("jammu","srinagar",4);
+    g.addEdge("jammu","laddak",5);
+    g.addEdge("chandigardh","jammu",8);
+    g.addEdge("laddak","srinagar",12);
+    g.addEdge("kolkata","gowahati",7);
+    g.addEdge("sikkim","gowahati",5);
+    g.addEdge("beguluru","kolkata",6);
+    g.addEdge("beguluru","chennai",4);
+    g.addEdge("beguluru","mumbai",4);
+    g.addEdge("jaipur","mumbai",5);
+    g.addEdge("jaipur","bhopal",3);
+    g.addEdge("delhi","bhopal",3);
+    g.dijkstra("delhi");
 
 
 }
