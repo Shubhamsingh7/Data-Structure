bb99d9999bc6d9060a74000657556ca050aaa7f8
Shubham singh <singh6shubham@gmail.com>
2020-05-13 04:15:13 +0530

    bfs + dfs in matrix form

--- 06Graph/matrixform/graph.cpp
+++ 06Graph/matrixform/graph.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
+#include<queue>
 #include "declaration.h"
 using namespace std;
 
 static int graph[100][100];
 static int n = 4;
+bool visited[4];
 
 int main(){
 
@@ -14,16 +16,20 @@ int main(){
     addedge(1,3,8);
     addedge(3,4,7);
     addedge(3,3,2);
+    addedge(1,2,2);
 
     displayGraph();
-    // dfs({4,3});
-    // bfs({1,2});
+    // cout<<"dfs :- ";
+    // dfs(2);
+    cout<<"bfs :- ";
+    bfs(1);
     // dijkstra({2,3});
 
 }
 
 void createGraph(){
     for(int i = 0 ; i < n ; i++){
+        visited[i] = false;
         for(int j = 0 ; j < n ; j++){
             graph[i][j] = 0;
         }
@@ -32,7 +38,7 @@ void createGraph(){
 
 void addedge(int source ,int destination,int weight){
     graph[source][destination] = weight;
-    graph[destination][source] = weight;
+    // graph[destination][source] = weight;
 }
 
 void displayGraph(){
@@ -42,6 +48,41 @@ void displayGraph(){
         }
         cout<<endl;
     }
+}
+
+// dfs
+
+void dfs(int source){
+    cout<<source<<" ";
+    visited[source] = true;
+    for(int i = 0 ; i < n ; i++){
+        if(graph[source][i] != 0 && !visited[i]){
+            dfs(i);
+        }
+    }
+
+}
+
+void bfsHelper(queue<int> &q){
+    while(!q.empty()){
+        int currentElement = q.front();
+        q.pop();
+        cout<<currentElement<<" ";
+        // visited[currentElement] = true;
+        for(int i = 0 ; i < n ; i++){
+            if(graph[currentElement][i] != 0 && !visited[i]){
+                q.push(i);
+                visited[i] = true;
+            }
+        }
+    }
+}
+
+void bfs(int source){
+    queue<int> q;
+    q.push(source);
+    visited[source] = true;
+    bfsHelper(q);
 }
 
 
