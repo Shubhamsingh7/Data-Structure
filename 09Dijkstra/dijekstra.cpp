e7482031f719059e2e476a3b0a69f1ed2d14def4
Shubham singh <singh6shubham@gmail.com>
2020-05-13 08:10:12 +0530

    dijkstra algorithm

--- 09Dijkstra/dijekstra.cpp
+++ 09Dijkstra/dijekstra.cpp
@@ -53,13 +53,6 @@ class Graph{
 };
 int main(){
 
-    // Graph<int> g;
-    // g.addEdge(1,2,1);
-    // g.addEdge(2,3,8);
-    // g.addEdge(1,3,4);
-    // g.addEdge(1,4,7);
-    // g.addEdge(3,4,2);
-    // g.dijkstra(1);
     
     Graph<string> g;
     g.addEdge("delhi","lucknow",5);
