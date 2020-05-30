88c674f2db734badc40dcf6010e3f21b41c81b96
Shubham singh <singh6shubham@gmail.com>
2020-05-30 18:41:32 +0530

    segment tree

--- 14AdvanceDS/SegmentTree.cpp
+++ 14AdvanceDS/SegmentTree.cpp
@@ -1,10 +1,7 @@
-// minimum range query
 #include<iostream>
 using namespace std;
 
-//O(n) time
-void buildSegmentTree(int arr[],int tree[],int index,int start,int end){
-
+void buildTree(int *arr,int *tree,int index,int start,int end){
     if(start > end){
         return;
     }
@@ -13,73 +10,85 @@ void buildSegmentTree(int arr[],int tree[],int index,int start,int end){
         return;
     }
     int mid = (start + end)/2;
-    int indexLeft = 2 * index + 1;
-    int indexRight = 2 * index + 2;
 
-    buildSegmentTree(arr,tree,indexLeft,start,mid);
-    buildSegmentTree(arr,tree,indexRight,mid+1,end);
-    tree[index] = min(tree[indexLeft],tree[indexRight]);
+    buildTree(arr,tree,2*index + 1,start,mid);
+    buildTree(arr,tree,2*index+2,mid+1,end);
+    int left = tree[2*index+1];
+    int right = tree[2*index+2];
+    tree[index] = min(left,right);
     return;
 }
 
-//Query  O(log n)
-int query(int tree[],int index,int start,int end,int qStart,int qEnd){
-    // no overlap
-    if(qStart > end || qEnd < start){
+// update node
+
+void updateNode(int *tree,int *arr,int index,int start,int end,int updateIndex,int element){
+    if((updateIndex > end) || (updateIndex < start)){
+        return;
+    }
+    if(start == end){
+        tree[index] = element;
+        return;
+    }
+    int mid = (start + end)/2;
+    updateNode(tree,arr,2*index+1,start,mid,updateIndex,element);
+    updateNode(tree,arr,2*index+2,mid+1,end,updateIndex,element);
+    tree[index] = min(tree[2*index+1],tree[2*index+2]);
+    return;
+}
+
+// update range
+
+void updateRange(int *tree,int *arr,int index,int start,int end,int ustart,int uend,int element){
+    //Zero overlap
+    if((start > uend) || (end < ustart)){
+        return;
+    }
+    if(start == end){
+        tree[index] += element;
+        return;
+    }
+
+    int mid = (start + end)/2;
+    updateRange(tree,arr,2*index+1,start,mid,ustart,uend,element);
+    updateRange(tree,arr,2*index+2,mid+1,end,ustart,uend,element);
+    tree[index] = min(tree[2*index+1],tree[2*index+2]);
+    return;
+}
+
+
+int query(int *tree,int index,int start,int end,int qstart,int qend){
+    //Zero overlap
+    if((start > qend) || (end < qstart)){
         return INT_MAX;
     }
-    // full overlap
-    if(qStart <= start && qEnd >= end){ 
+    // Full overlap
+    if(start >= qstart && end <= qend){
         return tree[index];
     }
-
-    // partial overlap
     int mid = (start + end)/2;
-    int indexLeft = 2 * index + 1;
-    int indexRight = 2 * index + 2;
-
-    int leftMin = query(tree,indexLeft,start,mid,qStart,qEnd);
-    int rightMin = query(tree,indexRight,mid + 1,end,qStart,qEnd);
-    return min(leftMin,rightMin);
+    int left = query(tree,2*index+1,start,mid,qstart,qend);
+    int right = query(tree,2*index+2,mid+1,end,qstart,qend);
+    return min(left,right);
 
 }
-
-
-// update Node
-void updateNode(int tree[],int index,int start,int end,int val){
-    // full overlap
-    if(start == end){
-        tree[index] = val;
-        return;
-    }
-    // no overlap
-    if(start > end){ 
-        return;
-    }
-
-    // partial overlap
-    
-    int mid = (start + end)/2;
-    int indexLeft = 2 * index + 1;
-    int indexRight = 2 * index + 2;
-
-    updateNode(tree,indexLeft,start,mid,val);
-    updateNode(tree,indexRight,mid + 1,end,val);
-    return;
-
-}
-
-
 
 int main(){
-    int arr[] {3,7,2,7,5,6,3,1};
-    int length = sizeof(arr)/sizeof(int);
-    int tree[4*length];
+    int arr[] = {10,8,9,8,24,45,2,5,7,63};
+    int length = sizeof(arr)/sizeof(arr[0]);
+    int *tree = new int[4*length];
     int index = 0;
     int start = 0;
-    int end = length-1;
-    buildSegmentTree(arr,tree,index,start,end);
+    int end = (length - 1);
+    buildTree(arr,tree,index,start,end);
+        cout<<query(tree,index,start,end,start,end)<<endl;
+        cout<<query(tree,index,start,end,4,end)<<endl;
+        cout<<query(tree,index,start,end,1,3)<<endl;
+        updateNode(tree,arr,index,start,end,5,-10);
+        cout<<query(tree,index,start,end,start,end)<<endl;
+        updateRange(tree,arr,start,start,end,start,5,100);
+        cout<<query(tree,index,start,end,start,end)<<endl;
+
+        
     
-    cout<<query(tree,0,0,length-1,3,6);
     return 0;
 }
